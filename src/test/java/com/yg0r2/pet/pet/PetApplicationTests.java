df551f789cf14622b5eb746337f3fab10c74a3c1
Yg0R2 <yg0r2@yahoo.com>
2019-11-01 12:30:04 +0100

    Initial commit

---
+++ src/test/java/com/yg0r2/pet/pet/PetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yg0r2.pet.pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
