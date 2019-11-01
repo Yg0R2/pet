1ebc61b4bc23ffa37ea5415448fec9b8913cca1a
Yg0R2 <yg0r2@yahoo.com>
2019-11-01 20:13:36 +0100

    Fix Java compile warning

--- client/src/main/java/com/yg0r2/pet/client/DefaultPetClient.java
+++ client/src/main/java/com/yg0r2/pet/client/DefaultPetClient.java
@@ -29,7 +29,7 @@ public class DefaultPetClient extends AbstractCoreClient implements PetClient {
     }
 
     public PetEntry get(long id, RequestContext requestContext) {
-        HttpEntity<?> entity = new HttpEntity(createHeaders(requestContext));
+        HttpEntity<?> entity = new HttpEntity<>(createHeaders(requestContext));
 
         return restTemplate.exchange(petClientConfig.getGetUrl(), HttpMethod.GET, entity, PetEntry.class, id)
             .getBody();
