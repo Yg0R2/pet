a7e00133aa6971f21a3ee4cf86eb59ac178784c7
Yg0R2 <yg0r2@yahoo.com>
2019-12-21 12:19:03 +0100

    Refactor ExceptionHandler to log exceptions

--- web/src/main/java/com/yg0r2/pet/web/rest/RestResponseEntityExceptionHandler.java
+++ web/src/main/java/com/yg0r2/pet/web/rest/RestResponseEntityExceptionHandler.java
@@ -3,6 +3,9 @@ package com.yg0r2.pet.web.rest;
 import com.yg0r2.pet.service.exceptions.PetEntryNotFoundException;
 import com.yg0r2.pet.service.exceptions.PetServiceInternalException;
 import com.yg0r2.pet.service.exceptions.UnableToCreatePetEntryException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,24 +15,27 @@ import org.springframework.web.servlet.mvc.annotation.ResponseStatusExceptionRes
 @RestControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseStatusExceptionResolver {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(RestResponseEntityExceptionHandler.class);
+
     @ExceptionHandler(value = PetEntryNotFoundException.class)
-    public ResponseEntity<String> handleNotFoundExceptions(RuntimeException runtimeException) {
-        return new ResponseEntity<>(runtimeException.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<String> handleNotFoundExceptions(PetEntryNotFoundException exception) {
+        LOGGER.error("Entry not found.", exception);
+
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(value = UnableToCreatePetEntryException.class)
-    public ResponseEntity<String> handleCreateExceptions(RuntimeException runtimeException) {
-        return new ResponseEntity<>(runtimeException.getMessage(), HttpStatus.FORBIDDEN);
+    public ResponseEntity<String> handleCreateExceptions(UnableToCreatePetEntryException exception) {
+        LOGGER.error("Entry creation failed.", exception);
+
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.FORBIDDEN);
     }
 
-    @ExceptionHandler(value = PetServiceInternalException.class)
-    public ResponseEntity<String> handleInternalException(RuntimeException runtimeException) {
-        return new ResponseEntity<>(runtimeException.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+    @ExceptionHandler(value = {PetServiceInternalException.class, RuntimeException.class})
+    public ResponseEntity<String> handleInternalException(PetServiceInternalException exception) {
+        LOGGER.error(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), exception);
 
-    @ExceptionHandler(value = RuntimeException.class)
-    public ResponseEntity<String> handleRuntimeException(RuntimeException runtimeException) {
-        return new ResponseEntity<>(runtimeException.getMessage(), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
