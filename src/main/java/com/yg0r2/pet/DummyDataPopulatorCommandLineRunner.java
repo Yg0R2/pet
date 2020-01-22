9744e4c1c1900fcadd617c303fcf6b44da0aa852
Tibor Kovacs (EPAM) <tibokovacs.contractor@libertyglobal.com>
2020-01-22 11:59:53 +0100

    Add dummy command line runner to populate default data

---
+++ src/main/java/com/yg0r2/pet/DummyDataPopulatorCommandLineRunner.java
@@ -0,0 +1,35 @@
+package com.yg0r2.pet;
+
+import com.yg0r2.user.dao.model.UserEntity;
+import com.yg0r2.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DummyDataPopulatorCommandLineRunner implements CommandLineRunner {
+
+    private static final String DEFAULT_USER_NICK_NAME = "test";
+    private static final String DEFAULT_USER_PASSWORD = "test";
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        UserEntity testUser = createDefaultUser();
+    }
+
+    private UserEntity createDefaultUser() {
+        UserEntity userEntity = new UserEntity();
+
+        userEntity.setNickName(DEFAULT_USER_NICK_NAME);
+        userEntity.setPassword(bCryptPasswordEncoder.encode(DEFAULT_USER_PASSWORD));
+
+        return userService.create(userEntity);
+    }
+
+}
