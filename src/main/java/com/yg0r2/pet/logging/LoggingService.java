cf72fa99703f10e8703e6e9d8a7540af0c9f60bd
Yg0R2 <yg0r2@yahoo.com>
2020-05-02 13:18:17 +0200

    Fix request/response header logging

--- src/main/java/com/yg0r2/pet/logging/LoggingService.java
+++ src/main/java/com/yg0r2/pet/logging/LoggingService.java
@@ -2,21 +2,20 @@ package com.yg0r2.pet.logging;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Iterator;
-import java.util.Map;
 import java.util.Spliterator;
 import java.util.Spliterators;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Component
 public final class LoggingService {
@@ -28,22 +27,24 @@ public final class LoggingService {
     private ObjectMapper objectMapper;
 
     public void logRequest(HttpServletRequest request, Object body) {
-        Map<String, String> headers = getHeaders(request.getHeaderNames().asIterator(), request::getHeader);
+        MultiValueMap<String, String> headers = getHeaders(request.getHeaderNames().asIterator(), request::getHeader);
 
         LOGGER.info(String.format(PATTERN, "Request", request.getMethod(), request.getRequestURL().toString(), headers, getBodyAsJson(body)));
     }
 
     public void logResponse(HttpServletRequest request, HttpServletResponse response, Object body) {
-        Map<String, String> headers = getHeaders(response.getHeaderNames().iterator(), response::getHeader);
+        MultiValueMap<String, String> headers = getHeaders(response.getHeaderNames().iterator(), response::getHeader);
 
         LOGGER.info(String.format(PATTERN, "Response", request.getMethod(), request.getRequestURL().toString(), headers, getBodyAsJson(body)));
     }
 
-    private Map<String, String> getHeaders(Iterator<String> headerNames, Function<String, String> valueMappingFunction) {
-        Spliterator<String> spliterator = Spliterators.spliteratorUnknownSize(headerNames, Spliterator.IMMUTABLE);
+    private MultiValueMap<String, String> getHeaders(Iterator<String> headerNames, Function<String, String> valueMappingFunction) {
+        MultiValueMap<String, String> headers = new HttpHeaders();
 
-        return StreamSupport.stream(spliterator, false)
-            .collect(Collectors.toMap(Function.identity(), valueMappingFunction));
+        StreamSupport.stream(Spliterators.spliteratorUnknownSize(headerNames, Spliterator.IMMUTABLE), false)
+            .forEach(headerName -> headers.add(headerName, valueMappingFunction.apply(headerName)));
+
+        return headers;
     }
 
     private String getBodyAsJson(Object body) {
